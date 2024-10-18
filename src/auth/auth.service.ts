c85026432e8f420875e0d53d211ec2f21f62809e
Jhonny <jhonnycgarcia@gmail.com>
2024-10-18 14:37:39 -0500

    Encriptar la contraseña

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,12 @@
 import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcryptjs';
+
+import { User } from './entities/user.entity';
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
-import { InjectModel } from '@nestjs/mongoose';
-import { User } from './entities/user.entity';
-import { Model } from 'mongoose';
 
 @Injectable()
 export class AuthService {
@@ -14,14 +17,24 @@ export class AuthService {
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     try {
+      const { password, ...userData } = createUserDto;
+      // const newUser = new this.userModel(createUserDto);
+
       // 1.- Encriptar la contraseña
+      const newUser = new this.userModel({
+        password: bcrypt.hashSync(password, 10),
+        ...userData,
+      });
       
       // 2.- Guardar el usuario
-      const newUser = new this.userModel(createUserDto);
-      return await newUser.save();
-  
+      
       // 3.- Generar el JWT
       
+      await newUser.save();
+
+      const { password: _password, ...user } = newUser.toObject();
+      return user;
+
     } catch (error) {
       if(error.code === 11000) {
         throw new BadRequestException(`${createUserDto.email} already exists`);
