1274853968d285ed922d4974c014db43e4a95242
Jhonny <jhonnycgarcia@gmail.com>
2024-10-18 12:53:10 -0500

    Conectar Nest con MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/27017'),
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
